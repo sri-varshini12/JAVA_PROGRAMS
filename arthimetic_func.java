4312e69a5d79d21ad27f0fb7f8f8cc796ec10cc3
Sri Varshini iyyappan <145577418+sri-varshini12@users.noreply.github.com>
2025-08-12 19:25:35 +0530

    Create arthimetic_func.java

---
+++ arthimetic_func.java
@@ -0,0 +1,15 @@
+package varshini_java;
+
+public class func_arthimetic {
+	void sum(int a,int b) {
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.print(a*b);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		func_arthimetic obj=new func_arthimetic();
+		obj.sum(20, 20);
+	}
+
+}
