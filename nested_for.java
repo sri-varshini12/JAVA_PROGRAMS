a0086e7f23ee598b60b8af09b136c75f0d65c955
Sri Varshini iyyappan <145577418+sri-varshini12@users.noreply.github.com>
2025-08-12 16:10:24 +0530

    Create nested_for.java

---
+++ nested_for.java
@@ -0,0 +1,16 @@
+package varshini_java;
+
+public class nestedforloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=1;i++) {
+			for(int j=1;j<=2;j++) {
+				System.out.println("varshini");
+				
+			}
+			
+		}
+	}
+
+}
