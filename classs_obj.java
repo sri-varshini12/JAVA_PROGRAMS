f8e4803d1751229473c6b32e86152d8fac30960f
Sri Varshini iyyappan <145577418+sri-varshini12@users.noreply.github.com>
2025-08-12 18:16:56 +0530

    Create classs_obj.java

---
+++ classs_obj.java
@@ -0,0 +1,22 @@
+package varshini_java;
+
+public class class_obj {
+	
+	String lap_name="";
+	String lap_proc="";
+	int lap_ram=0;
+	int lap_price=0;
+	String lap_owner="";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		creating object here 
+		class_obj objects=new class_obj(); 
+		objects.lap_name="lenovo";
+		objects.lap_owner="varshini";
+		objects.lap_price=40999;
+		System.out.print(objects.lap_name);
+
+	}
+
+}
