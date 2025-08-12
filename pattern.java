e5665d4847883136f2e6c73d512e638a6e4e3a15
Sri Varshini iyyappan <145577418+sri-varshini12@users.noreply.github.com>
2025-08-12 16:18:42 +0530

    Create pattern.java

---
+++ pattern.java
@@ -0,0 +1,16 @@
+package varshini_java;
+
+public class pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<=2;i++) {
+			for(int j=0;j<=2;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
