c1c86a53e0bb44db92ff40089302360bc69b847f
Sri Varshini iyyappan <145577418+sri-varshini12@users.noreply.github.com>
2025-08-12 16:37:43 +0530

    Create basic_whileloop.java

---
+++ basic_whileloop.java
@@ -0,0 +1,15 @@
+package varshini_java;
+
+public class whileloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5;
+		int i=1;
+		while(i<=n) {
+			System.out.println(i);
+			i++;
+		}
+	}
+
+}
